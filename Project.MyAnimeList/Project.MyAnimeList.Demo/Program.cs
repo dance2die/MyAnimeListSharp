59d013d03625c8ceff4d0d157f836e72f5ca08a6
dance2die <dance2die@hotmail.com>
2015-08-31 22:18:02 -0400

    Refactor: Extracted "AccountMethods"

--- Project.MyAnimeList/Project.MyAnimeList.Demo/Program.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Demo/Program.cs
@@ -18,28 +18,46 @@ namespace Project.MyAnimeList.Demo
 
 		private static void TestCredentials(ICredentialContext credential)
 		{
-			HttpWebRequest request = CreateCredentialWebRequest(credential);
-			string responseFromServer = GetResponseText(request);
+			//HttpWebRequest request = CreateCredentialWebRequest(credential);
+			//string responseFromServer = GetResponseText(request);
+			RequestParameters requestParameters = new VerifyCredentialsRequestParameters(credential);
+			string responseFromServer  = new AccountMethods(requestParameters).VerifyCredentials();
 
 			Console.WriteLine(responseFromServer);
-		}
-
-		private static string GetResponseText(HttpWebRequest request)
-		{
-			using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-			using (Stream responseStream = response.GetResponseStream())
-			using (StreamReader reader = new StreamReader(responseStream))
-			{
-				//Read the content.
-				string responseFromServer = reader.ReadToEnd();
-				return responseFromServer;
-			}
 		}
 
 		private static HttpWebRequest CreateCredentialWebRequest(ICredentialContext credential)
 		{
 			RequestParameters requestParameters = new VerifyCredentialsRequestParameters(credential);
 			return WebRequestBuilder.BuildWebRequest(requestParameters);
+		}
+	}
+
+	public class AccountMethods : MyAnimeListMethods
+	{
+		public AccountMethods(RequestParameters requestParameters)
+		{
+			RequestParameters = requestParameters;
+		}
+
+		public string VerifyCredentials()
+		{
+			return GetResponseText(WebRequestBuilder.BuildWebRequest(RequestParameters));
+		}
+	}
+
+	public abstract class MyAnimeListMethods
+	{
+		public RequestParameters RequestParameters { get; set; }
+
+		protected string GetResponseText(HttpWebRequest request)
+		{
+			using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+			using (Stream responseStream = response.GetResponseStream())
+			using (StreamReader reader = new StreamReader(responseStream))
+			{
+				return reader.ReadToEnd();
+			}
 		}
 	}
 }
