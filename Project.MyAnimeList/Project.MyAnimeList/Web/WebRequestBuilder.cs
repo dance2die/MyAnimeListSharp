824d3f967e608e24e471b6a9e6ae4a137763c332
dance2die <dance2die@hotmail.com>
2015-09-02 09:45:00 -0400

    Static class to regular internal class.

--- Project.MyAnimeList/Project.MyAnimeList/Web/WebRequestBuilder.cs
+++ Project.MyAnimeList/Project.MyAnimeList/Web/WebRequestBuilder.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Text;
+using Project.MyAnimeList.Util;
 
 namespace Project.MyAnimeList.Web
 {
-	internal static class WebRequestBuilder
+	internal class WebRequestBuilder
 	{
-		public static HttpWebRequest BuildWebRequest(RequestParameters requestParameters)
+		public RequestParameters RequestParameters { get; set; }
+
+		public WebRequestBuilder(RequestParameters requestParameters)
 		{
-			string requestUri = new RequestUriBuilder(requestParameters).GetRequestUri();
+			RequestParameters = requestParameters;
+		}
+
+		public HttpWebRequest BuildWebRequest()
+		{
+			string requestUri = new RequestUriBuilder(RequestParameters).GetRequestUri();
 			HttpWebRequest result = WebRequest.Create(requestUri) as HttpWebRequest;
 			if (result == null)
 				throw new InvalidOperationException("Could not create web request");
@@ -16,16 +27,35 @@ namespace Project.MyAnimeList.Web
 
 			// credit
 			// https://github.com/DeadlyEmbrace/MyAnimeListAPI/blob/master/MyAnimeListAPI/MyAnimeListAPI/Credentials.cs
-			result.Method = requestParameters.HttpMethod;
+			result.Method = RequestParameters.HttpMethod;
 			result.UseDefaultCredentials = false;
 			result.Credentials = new NetworkCredential(
-				requestParameters.Credential.UserName, requestParameters.Credential.Password);
+				RequestParameters.Credential.UserName, RequestParameters.Credential.Password);
+
 			// credit
 			// https://github.com/LHCGreg/mal-api/blob/f6c82c95d139807a1d6259200ec7622384328bc3/MalApi/MyAnimeListApi.cs
 			result.AutomaticDecompression = DecompressionMethods.GZip;
 
-			return result;
+			WritePostBody(result);
 
+			return result;
+		}
+
+		private void WritePostBody(HttpWebRequest request)
+		{
+			string postBody = GetEncodedString(RequestParameters.PostBodyProperties);
+			if (string.IsNullOrWhiteSpace(postBody)) return;
+
+			using (Stream requestStream = request.GetRequestStream())
+			{
+				byte[] content = Encoding.ASCII.GetBytes(postBody);
+				requestStream.Write(content, 0, content.Length);
+			}
+		}
+
+		private string GetEncodedString(IDictionary<string, string> dictionary)
+		{
+			return new DictionaryToStringJoiner().Join(dictionary);
 		}
 	}
 }
