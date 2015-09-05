743e80862d83bbbe7393e90ab7314b871974ec48
dance2die <dance2die@hotmail.com>
2015-09-05 17:13:51 -0400

    Updated Demo

--- Project.MyAnimeList/Project.MyAnimeList.Demo/Program.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Demo/Program.cs
@@ -8,19 +8,37 @@ namespace Project.MyAnimeList.Demo
 	{
 		public static void Main(string[] args)
 		{
-			ICredentialContext credential = new CredentialContext();
+			// Step 1: Enter UserName and Password information
+			ICredentialContext credential = new CredentialContext
+			{
+				UserName = "<MyAnimeList.NET UserName>",
+				Password = "<MyAnimeList.NET Password>"
+			};
 
-			TestCredentials(credential);
+			// Step 2: Create a method object
+			var searchMethods = new SearchMethods(credential);
+
+			// Step 3: Search using the search term ("Full Metal" in this case)
+			string mangaResponse = searchMethods.SearchManga("Code Geass");
+			Console.WriteLine(mangaResponse);
+
+			// Step 3: Search using the search term ("Full Metal" in this case)
+			string animeResponse = searchMethods.SearchAnime("Full Metal");
+			Console.WriteLine(animeResponse);
+
+
+			//TestCredentials(credential);
 
 			Console.WriteLine("Press ENTER to continue...");
 			Console.ReadLine();
 		}
 
-		private static void TestCredentials(ICredentialContext credential)
-		{
-			string responseFromServer  = new AccountMethods(credential).VerifyCredentials();
 
-			Console.WriteLine(responseFromServer);
-		}
+		//private static void TestCredentials(ICredentialContext credential)
+		//{
+		//	string responseFromServer  = new AccountMethods(credential).VerifyCredentials();
+
+		//	Console.WriteLine(responseFromServer);
+		//}
 	}
 }
