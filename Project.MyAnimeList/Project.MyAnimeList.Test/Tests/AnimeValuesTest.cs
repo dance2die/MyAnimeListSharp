82251defa68811d0001f71ca59f6cb328db85446
dance2die <dance2die@hotmail.com>
2015-09-07 15:34:53 -0400

    Added "AnimeValuesTest"

---
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeValuesTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using Project.MyAnimeList.Test.Fixture;
+
+namespace Project.MyAnimeList.Test.Tests
+{
+	public class AnimeValuesTest : CredentialCollectionTest
+	{
+		public AnimeValuesTest(CredentialContextFixture credentialContextFixture) 
+			: base(credentialContextFixture)
+		{
+		}
+
+
+	}
+}
