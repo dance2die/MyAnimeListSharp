4efa73131a5eccc7e4609786b428e4bb21239b7e
dance2die <dance2die@hotmail.com>
2015-09-07 16:28:25 -0400

    Tested generating xml declaration

--- Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeValuesTest.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeValuesTest.cs
@@ -1,15 +1,54 @@
-﻿using Xunit;
+﻿using System;
+using System.Xml.Linq;
+using Xunit;
 using Project.MyAnimeList.Test.Fixture;
+using Xunit.Abstractions;
 
 namespace Project.MyAnimeList.Test.Tests
 {
 	public class AnimeValuesTest : CredentialCollectionTest
 	{
-		public AnimeValuesTest(CredentialContextFixture credentialContextFixture) 
+		private const string XML_DECLARATION = @"<?xml version=""1.0"" encoding=""UTF-8""?>";
+
+		private readonly ITestOutputHelper _output;
+
+		private readonly string _data =
+				XML_DECLARATION +
+			@"<entry>
+					<episode>9</episode>
+					<status>1</status>
+					<score>9</score>
+					<downloaded_episodes></downloaded_episodes>
+					<storage_type></storage_type>
+					<storage_value></storage_value>
+					<times_rewatched></times_rewatched>
+					<rewatch_value></rewatch_value>
+					<date_start></date_start>
+					<date_finish></date_finish>
+					<priority></priority>
+					<enable_discussion></enable_discussion>
+					<enable_rewatching></enable_rewatching>
+					<comments></comments>
+					<fansub_group></fansub_group>
+					<tags>test tag, 2nd tag</tags>
+				</ entry>";
+
+		public AnimeValuesTest(CredentialContextFixture credentialContextFixture, ITestOutputHelper output)
 			: base(credentialContextFixture)
 		{
+			_output = output;
 		}
 
+		[Fact]
+		public void TestXmlDeclration()
+		{
+			string standalone = null;
+			var doc = new XDocument(new XDeclaration("1.0", "UTF-8", standalone));
 
+			var declaration = doc.Declaration.ToString();
+			_output.WriteLine(XML_DECLARATION);
+			_output.WriteLine(declaration);
+			Assert.True(string.Compare(XML_DECLARATION, declaration, StringComparison.InvariantCultureIgnoreCase) == 0);
+		}
 	}
 }
