53c0e708f7121116ef73b929b28b7078d4819dd6
dance2die <dance2die@hotmail.com>
2015-09-10 19:01:20 -0400

    Set up "ITestOutputHelper"

--- Project.MyAnimeList/Project.MyAnimeList.Test/Tests/MangaValuesTest.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/MangaValuesTest.cs
@@ -1,14 +1,41 @@
 ﻿using Project.MyAnimeList.Test.Fixture;
+using Xunit.Abstractions;
 
 namespace Project.MyAnimeList.Test.Tests
 {
 	public class MangaValuesTest : CredentialCollectionTest
 	{
-		public MangaValuesTest(CredentialContextFixture credentialContextFixture) 
+		private const string XML_DECLARATION = @"<?xml version=""1.0"" encoding=""UTF-8""?>";
+
+		private readonly ITestOutputHelper _output;
+		//private readonly MangaValues _sut;
+
+		private readonly string _data =
+				XML_DECLARATION +
+			@"<entry>
+				<chapter>6</chapter>
+				<volume>1</volume>
+				<status>1</status>
+				<score>8</score>
+				<downloaded_chapters></downloaded_chapters>
+				<times_reread></times_reread>
+				<reread_value></reread_value>
+				<date_start></date_start>
+				<date_finish></date_finish>
+				<priority></priority>
+				<enable_discussion></enable_discussion>
+				<enable_rereading></enable_rereading>
+				<comments></comments>
+				<scan_group></scan_group>
+				<tags></tags>
+				<retail_volumes></retail_volumes>
+			</entry>";
+
+
+		public MangaValuesTest(CredentialContextFixture credentialContextFixture, ITestOutputHelper output) 
 			: base(credentialContextFixture)
 		{
+			_output = output;
 		}
-
-
 	}
 }
