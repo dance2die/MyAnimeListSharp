2860369ba80ed2b5fc4be8ff09665b37c5aa61e4
dance2die <dance2die@hotmail.com>
2015-09-12 12:30:15 -0400

    Removed _data property to "AnimeListMethodsTest"

--- Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeValuesTest.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeValuesTest.cs
@@ -16,27 +16,6 @@ namespace Project.MyAnimeList.Test.Tests
 		private readonly ITestOutputHelper _output;
 		private readonly AnimeValues _sut;
 
-		private readonly string _data =
-				XML_DECLARATION +
-			@"<entry>
-					<episode>1</episode>
-					<status>2</status>
-					<score>3</score>
-					<downloaded_episodes>4</downloaded_episodes>
-					<storage_type>5</storage_type>
-					<storage_value>6</storage_value>
-					<times_rewatched>7</times_rewatched>
-					<rewatch_value>8</rewatch_value>
-					<date_start>01302015</date_start>
-					<date_finish>07312015</date_finish>
-					<priority>1</priority>
-					<enable_discussion>1</enable_discussion>
-					<enable_rewatching>1</enable_rewatching>
-					<comments>This is a comment</comments>
-					<fansub_group>Horrible Subs</fansub_group>
-					<tags>Test Tag1, Test Tag2</tags>
-				</ entry>";
-
 		public AnimeValuesTest(CredentialContextFixture credentialContextFixture, ITestOutputHelper output)
 			: base(credentialContextFixture)
 		{
