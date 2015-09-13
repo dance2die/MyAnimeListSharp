0332607b7ab29f2069ffb20ee23c1871747cbb04
dance2die <dance2die@hotmail.com>
2015-09-12 20:54:22 -0400

    Extracted ValuesFormatterTest

--- Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeValuesFormatterTest.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeValuesFormatterTest.cs
@@ -1,43 +1,9 @@
-﻿using System.IO;
-using System.Xml;
-using Microsoft.XmlDiffPatch;
-using MyAnimeListSharp.Facade;
+﻿using MyAnimeListSharp.Facade;
 using Project.MyAnimeList.Test.Fixture;
 using Xunit;
 
 namespace Project.MyAnimeList.Test.Tests
 {
-	public abstract class ValuesFormatterTest :
-		IClassFixture<AnimeValuesFormatterTestFixture>
-		//IClassFixture<MangaValuesFormatterTestFixture>
-	{
-		/// <summary>
-		/// Compares two XML files to see if they are the same.
-		/// </summary>
-		/// <returns>
-		/// Returns true if two XML files are functionally identical, ignoring comments, white space, and child order.
-		/// </returns>
-		/// <remarks>http://stackoverflow.com/a/19954063/4035</remarks>
-		protected static bool XmlFilesAreIdentical(string file1, string file2)
-		{
-			var xmldiff = new XmlDiff();
-			var r1 = XmlReader.Create(new StringReader(file1));
-			var r2 = XmlReader.Create(new StringReader(file2));
-			var sw = new StringWriter();
-			var xw = new XmlTextWriter(sw) { Formatting = Formatting.Indented };
-
-			xmldiff.Options = XmlDiffOptions.IgnorePI |
-			                  XmlDiffOptions.IgnoreChildOrder |
-			                  XmlDiffOptions.IgnoreComments |
-			                  XmlDiffOptions.IgnoreWhitespace;
-			bool areIdentical = xmldiff.Compare(r1, r2, xw);
-
-			string differences = sw.ToString();
-
-			return areIdentical;
-		}
-	}
-
 	public class AnimeValuesFormatterTest : ValuesFormatterTest, IClassFixture<AnimeValuesFixture>
 	{
 		private readonly AnimeValuesFormatterTestFixture _formatterFixture;
@@ -103,7 +69,7 @@ namespace Project.MyAnimeList.Test.Tests
 			AnimeValues values = _animeValuesFixture.Values;
 			string xmlString = _formatterFixture.ValuesFormatter.Format(values);
 
-			var thatXmlAreIdentical = ValuesFormatterTest.XmlFilesAreIdentical(_animeData, xmlString);
+			var thatXmlAreIdentical = XmlFilesAreIdentical(_animeData, xmlString);
 			Assert.True(thatXmlAreIdentical);
 		}
 
---
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/ValuesFormatterTest.cs
@@ -0,0 +1,39 @@
+using System.IO;
+using System.Xml;
+using Microsoft.XmlDiffPatch;
+using Project.MyAnimeList.Test.Fixture;
+using Xunit;
+
+namespace Project.MyAnimeList.Test.Tests
+{
+	public abstract class ValuesFormatterTest :
+		IClassFixture<AnimeValuesFormatterTestFixture>
+		//IClassFixture<MangaValuesFormatterTestFixture>
+	{
+		/// <summary>
+		/// Compares two XML files to see if they are the same.
+		/// </summary>
+		/// <returns>
+		/// Returns true if two XML files are functionally identical, ignoring comments, white space, and child order.
+		/// </returns>
+		/// <remarks>http://stackoverflow.com/a/19954063/4035</remarks>
+		protected static bool XmlFilesAreIdentical(string file1, string file2)
+		{
+			var xmldiff = new XmlDiff();
+			var r1 = XmlReader.Create(new StringReader(file1));
+			var r2 = XmlReader.Create(new StringReader(file2));
+			var sw = new StringWriter();
+			var xw = new XmlTextWriter(sw) { Formatting = Formatting.Indented };
+
+			xmldiff.Options = XmlDiffOptions.IgnorePI |
+			                  XmlDiffOptions.IgnoreChildOrder |
+			                  XmlDiffOptions.IgnoreComments |
+			                  XmlDiffOptions.IgnoreWhitespace;
+			bool areIdentical = xmldiff.Compare(r1, r2, xw);
+
+			string differences = sw.ToString();
+
+			return areIdentical;
+		}
+	}
+}
