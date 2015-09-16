3a01d85b4b81a3a1aa37000b58e11d4e6b71bd90
dance2die <dontbothermewithspam@gmail.com>
2015-09-16 10:33:54 -0400

    Removed unused code.

--- Project.MyAnimeList/Project.MyAnimeList.Test/Tests/MangaValuesTest.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/MangaValuesTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using MyAnimeListSharp.Core;
 using MyAnimeListSharp.Enums;
-using MyAnimeListSharp.Facade;
 using Project.MyAnimeList.Test.Fixture;
 using Xunit;
 using Xunit.Abstractions;
@@ -15,28 +14,6 @@ namespace Project.MyAnimeList.Test.Tests
 
 		private readonly ITestOutputHelper _output;
 		private readonly MangaValues _sut;
-
-		private readonly string _data =
-			XML_DECLARATION +
-			@"<entry>
-				<volume>6</volume>
-				<volume>1</volume>
-				<status>1</status>
-				<score>8</score>
-				<downloaded_chapters></downloaded_chapters>
-				<times_reread></times_reread>
-				<reread_value></reread_value>
-				<date_start></date_start>
-				<date_finish></date_finish>
-				<priority></priority>
-				<enable_discussion></enable_discussion>
-				<enable_rereading></enable_rereading>
-				<comments></comments>
-				<scan_group></scan_group>
-				<tags></tags>
-				<retail_volumes></retail_volumes>
-			</entry>";
-
 
 		public MangaValuesTest(CredentialContextFixture credentialContextFixture, ITestOutputHelper output)
 			: base(credentialContextFixture)
