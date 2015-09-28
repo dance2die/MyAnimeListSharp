26928b19d4007057ef5722984c0e2e7f7884b6db
dance2die <dontbothermewithspam@gmail.com>
2015-09-28 10:40:57 -0400

    added code comments for classes under "Facade"

--- Project.MyAnimeList/Project.MyAnimeList/Facade/AccountMethods.cs
+++ Project.MyAnimeList/Project.MyAnimeList/Facade/AccountMethods.cs
@@ -4,6 +4,10 @@ using MyAnimeListSharp.Parameters;
 
 namespace MyAnimeListSharp.Facade
 {
+	/// <summary>
+	/// Represents a "Account Methods" section in MyAnimeList.net specification.
+	/// <see cref="http://myanimelist.net/modules.php?go=api"/>
+	/// </summary>
 	public class AccountMethods : MyAnimeListMethods
 	{
 		public AccountMethods(ICredentialContext credentialContext)
@@ -11,6 +15,10 @@ namespace MyAnimeListSharp.Facade
 		{
 		}
 
+		/// <summary>
+		/// Check if the credential is valid or not
+		/// </summary>
+		/// <returns>Response from the server in raw string</returns>
 		public string VerifyCredentials()
 		{
 			return GetResponseText(new VerifyCredentialsRequestParameters(CredentialContext));
--- Project.MyAnimeList/Project.MyAnimeList/Facade/SearchMethods.cs
+++ Project.MyAnimeList/Project.MyAnimeList/Facade/SearchMethods.cs
@@ -5,6 +5,10 @@ using MyAnimeListSharp.Util;
 
 namespace MyAnimeListSharp.Facade
 {
+	/// <summary>
+	/// Represents a "Search Methods" section in MyAnimeList.net specification.
+	/// <see cref="http://myanimelist.net/modules.php?go=api"/>
+	/// </summary>
 	public class SearchMethods : MyAnimeListMethods
 	{
 		public SearchMethods(ICredentialContext credentialContext)
@@ -12,22 +16,42 @@ namespace MyAnimeListSharp.Facade
 		{
 		}
 
+		/// <summary>
+		/// Search anime by the given search term.
+		/// </summary>
+		/// <param name="searchTerm">Anime to search</param>
+		/// <returns>XML response in string format</returns>
 		public string SearchAnime(string searchTerm)
 		{
 			return GetResponseText(new AnimeSearchRequestParameters(CredentialContext, searchTerm));
 		}
 
+		/// <summary>
+		/// Search anime by the given search term.
+		/// </summary>
+		/// <param name="searchTerm">Anime to search</param>
+		/// <returns>XML response deserialized into an object</returns>
 		public AnimeSearchResponse SearchAnimeDeserialized(string searchTerm)
 		{
 			var responseString = SearchAnime(searchTerm);
 			return new SearchResponseDeserializer<AnimeSearchResponse>().Deserialize(responseString);
 		}
 
+		/// <summary>
+		/// Search manga by the given search term.
+		/// </summary>
+		/// <param name="searchTerm">Manga to search</param>
+		/// <returns>XML response in string format</returns>
 		public string SearchManga(string searchTerm)
 		{
 			return GetResponseText(new MangaSearchRequestParameters(CredentialContext, searchTerm));
 		}
 
+		/// <summary>
+		/// Search manga by the given search term.
+		/// </summary>
+		/// <param name="searchTerm">Manga to search</param>
+		/// <returns>XML response deserialized into an object</returns>
 		public MangaSearchResponse SearchMangaDeserialized(string searchTerm)
 		{
 			var responseString = SearchManga(searchTerm);
