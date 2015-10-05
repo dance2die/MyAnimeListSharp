7f1d575bee12e53c48c9f0b1646270c7a9675c44
dance2die <dontbothermewithspam@gmail.com>
2015-10-05 10:54:21 -0400

    Added "SearchMethodsAsyncTest" to create asynchronous version of SearchMethods

---
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/SearchMethodsAsyncTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.MyAnimeList.Test.Fixture;
+
+namespace Project.MyAnimeList.Test.Tests
+{
+	public class SearchMethodsAsyncTest : CredentialCollectionTest
+	{
+		public SearchMethodsAsyncTest(CredentialContextFixture credentialContextFixture) 
+			: base(credentialContextFixture)
+		{
+		}
+
+
+	}
+}
