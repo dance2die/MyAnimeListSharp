f1f0f2bf9df31a19d7edb9772805218bf5787c87
dance2die <dontbothermewithspam@gmail.com>
2015-10-05 14:39:32 -0400

    Added "AnimeSearchMethods"

---
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeSearchMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.MyAnimeList.Test.Fixture;
+
+namespace Project.MyAnimeList.Test.Tests
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <remarks>
+	/// <see cref="http://stackoverflow.com/a/23004036/4035"/>
+	/// </remarks>
+	public class AnimeSearchMethods : CredentialCollectionTest
+	{
+		public AnimeSearchMethods(CredentialContextFixture credentialContextFixture) 
+			: base(credentialContextFixture)
+		{
+		}
+	}
+}
