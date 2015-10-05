19a54bff49da9b3dacbf3b9956b2693b12ff9260
dance2die <dontbothermewithspam@gmail.com>
2015-10-05 14:46:27 -0400

    comments added.

--- Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeSearchMethods.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeSearchMethods.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Project.MyAnimeList.Test.Fixture;
+﻿using Project.MyAnimeList.Test.Fixture;
 
 namespace Project.MyAnimeList.Test.Tests
 {
 	/// <summary>
-	/// 
+	/// Test to create AnimeSearchMethods, an asynchronous part of SearchMethods facade.
 	/// </summary>
 	/// <remarks>
+	/// Create asynchronous versions of WebRequest/Response methods using following links.
+	/// <see cref="https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&es_th=1&ie=UTF-8#q=task.factory.fromasync%20webrequest&es_th=1"/>
 	/// <see cref="http://stackoverflow.com/a/23004036/4035"/>
+	/// <see cref="http://stackoverflow.com/a/14098942/4035"/>
+	/// <see cref="http://stackoverflow.com/q/17664191/4035"/>
 	/// </remarks>
 	public class AnimeSearchMethods : CredentialCollectionTest
 	{
