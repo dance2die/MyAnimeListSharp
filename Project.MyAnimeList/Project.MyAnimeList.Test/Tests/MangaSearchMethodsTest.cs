7bfd3bb13f3e55090a2b56337262f14ddfbdeee2
dance2die <dontbothermewithspam@gmail.com>
2015-10-09 14:55:20 -0400

    Implemented "MangaSearchMethods.SearchDeserializedAsync"

--- Project.MyAnimeList/Project.MyAnimeList.Test/Tests/MangaSearchMethodsTest.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/MangaSearchMethodsTest.cs
@@ -1,4 +1,5 @@
-﻿using MyAnimeListSharp.Facade.Async;
+﻿using MyAnimeListSharp.Core;
+using MyAnimeListSharp.Facade.Async;
 using Project.MyAnimeList.Test.Fixture;
 using Xunit;
 using Xunit.Abstractions;
@@ -26,6 +27,29 @@ namespace Project.MyAnimeList.Test.Tests
 
 			_output.WriteLine(response);
 			Assert.False(string.IsNullOrWhiteSpace(response));
+		}
+
+		[Theory]
+		[InlineData("Attack on Titan")]
+		[InlineData("Code Geass")]
+		[InlineData("Naruto")]
+		public async void SearchDeserializedAsyncResponseHasEntries(string searchTerm)
+		{
+			MangaSearchResponse response = await _sut.SearchDeserializedAsync(searchTerm);
+
+			_output.WriteLine($"response.Entries.Count = {response.Entries.Count}");
+			Assert.True(response.Entries.Count > 0);
+		}
+
+		[Theory]
+		[InlineData("a;dlsfjasdf")]
+		[InlineData("xyzopqrstu")]
+		public async void SearchDeserializedAsyncResponseHasNoEntry(string searchTerm)
+		{
+			MangaSearchResponse response = await _sut.SearchDeserializedAsync(searchTerm);
+
+			_output.WriteLine($"response.Entries.Count = {response.Entries.Count}");
+			Assert.True(response.Entries.Count == 0);
 		}
 	}
 }
--- Project.MyAnimeList/Project.MyAnimeList/Facade/Async/MangaSearchMethods.cs
+++ Project.MyAnimeList/Project.MyAnimeList/Facade/Async/MangaSearchMethods.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using MyAnimeListSharp.Auth;
 using MyAnimeListSharp.Core;
 using MyAnimeListSharp.Parameters;
+using MyAnimeListSharp.Util;
 
 namespace MyAnimeListSharp.Facade.Async
 {
@@ -12,10 +13,19 @@ namespace MyAnimeListSharp.Facade.Async
 		{
 		}
 
-
 		public async Task<string> SearchAsync(string searchTerm)
 		{
 			return await GetResponseTextAsync(new MangaSearchRequestParameters(CredentialContext, searchTerm));
+		}
+
+		public async Task<MangaSearchResponse> SearchDeserializedAsync(string searchTerm)
+		{
+			// ConfigureAwait(false) => There is no need to overload the workload by marshalling the current context.
+			var response = await SearchAsync(searchTerm).ConfigureAwait(false);
+			if (string.IsNullOrWhiteSpace(response))
+				return new MangaSearchResponse();
+
+			return await Task.Run(() => new SearchResponseDeserializer<MangaSearchResponse>().Deserialize(response));
 		}
 	}
 }
