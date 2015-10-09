5a220aaaac5ce89d913c2c33af1ef21c64cec161
dance2die <dance2die@hotmail.com>
2015-10-08 22:48:44 -0400

    added "MangaSearchMethodsTest"

---
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/MangaSearchMethodsTest.cs
@@ -0,0 +1,16 @@
+﻿using Project.MyAnimeList.Test.Fixture;
+using Xunit.Abstractions;
+
+namespace Project.MyAnimeList.Test.Tests
+{
+	public class MangaSearchMethodsTest : CredentialCollectionTest
+	{
+		private readonly ITestOutputHelper _output;
+
+		public MangaSearchMethodsTest(CredentialContextFixture credentialContextFixture, ITestOutputHelper output) 
+			: base(credentialContextFixture)
+		{
+			_output = output;
+		}
+	}
+}
