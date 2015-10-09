33e7810fa2199ba1b6b03b0b4aaa41936b1f614c
dance2die <dontbothermewithspam@gmail.com>
2015-10-09 15:48:05 -0400

    created "AccountMethodsAsyncTest"

---
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AccountMethodsAsyncTest.cs
@@ -0,0 +1,16 @@
+﻿using Project.MyAnimeList.Test.Fixture;
+using Xunit.Abstractions;
+
+namespace Project.MyAnimeList.Test.Tests
+{
+	public class AccountMethodsAsyncTest : CredentialCollectionTest
+	{
+		private readonly ITestOutputHelper _output;
+
+		public AccountMethodsAsyncTest(CredentialContextFixture credentialContextFixture, ITestOutputHelper output)
+			: base(credentialContextFixture)
+		{
+			_output = output;
+		}
+	}
+}
