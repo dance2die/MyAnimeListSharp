ff30e291ccaf28fd4a32df223108cd07c7f5408b
dance2die <dance2die@hotmail.com>
2015-10-10 11:11:44 -0400

    Moved common Anime test data to AnimeValuesFixture so that common test data can be shared to Async version as well.

--- Project.MyAnimeList/Project.MyAnimeList.Test/Fixture/AnimeValuesFixture.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Fixture/AnimeValuesFixture.cs
@@ -8,6 +8,37 @@ namespace Project.MyAnimeList.Test.Fixture
 	{
 		public AnimeValues Values { get; set; }
 
+		/// <summary>
+		/// Gate: Jieitai Kanochi nite, Kaku Tatakaeri
+		/// http://myanimelist.net/anime/28907/Gate:_Jieitai_Kanochi_nite_Kaku_Tatakaeri
+		/// </summary>
+		public static int AnimeId { get; set; } = 28907;
+
+		public static string XmlDeclaration { get; set; } = @"<?xml version=""1.0"" encoding=""UTF-8""?>";
+
+		public static readonly string Data = 
+			XmlDeclaration +
+			@"
+				<entry>
+					<episode>1</episode>
+					<status>2</status>
+					<score>3</score>
+					<downloaded_episodes>4</downloaded_episodes>
+					<storage_type>5</storage_type>
+					<storage_value>6</storage_value>
+					<times_rewatched>7</times_rewatched>
+					<rewatch_value>8</rewatch_value>
+					<date_start>01302015</date_start>
+					<date_finish>07312015</date_finish>
+					<priority>1</priority>
+					<enable_discussion>1</enable_discussion>
+					<enable_rewatching>1</enable_rewatching>
+					<comments>This is a comment</comments>
+					<fansub_group>Horrible Subs</fansub_group>
+					<tags>Test Tag1, Test Tag2</tags>
+				</entry>";
+
+
 		public AnimeValuesFixture()
 		{
 			Values = GetTestAnimeValues();
--- Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeListMethodsTest.cs
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/AnimeListMethodsTest.cs
@@ -17,34 +17,6 @@ namespace Project.MyAnimeList.Test.Tests
 		private readonly AnimeValuesFixture _animeValuesFixture;
 		private readonly AnimeListMethodsFixture _animeListMethodsFixture;
 
-		/// <summary>
-		/// Gate: Jieitai Kanochi nite, Kaku Tatakaeri
-		/// http://myanimelist.net/anime/28907/Gate:_Jieitai_Kanochi_nite_Kaku_Tatakaeri
-		/// </summary>
-		private const int ID = 28907;
-		private const string XML_DECLARATION = @"<?xml version=""1.0"" encoding=""UTF-8""?>";
-
-		private static readonly string _data = XML_DECLARATION +
-			@"
-				<entry>
-					<episode>1</episode>
-					<status>2</status>
-					<score>3</score>
-					<downloaded_episodes>4</downloaded_episodes>
-					<storage_type>5</storage_type>
-					<storage_value>6</storage_value>
-					<times_rewatched>7</times_rewatched>
-					<rewatch_value>8</rewatch_value>
-					<date_start>01302015</date_start>
-					<date_finish>07312015</date_finish>
-					<priority>1</priority>
-					<enable_discussion>1</enable_discussion>
-					<enable_rewatching>1</enable_rewatching>
-					<comments>This is a comment</comments>
-					<fansub_group>Horrible Subs</fansub_group>
-					<tags>Test Tag1, Test Tag2</tags>
-				</entry>";
-
 		public AnimeListMethodsTest(
 			CredentialContextFixture credentialContextFixture, 
 			ITestOutputHelper output, 
@@ -60,7 +32,7 @@ namespace Project.MyAnimeList.Test.Tests
 		[Fact]
 		public void AddAnimeRequestParametersMethodIsPost()
 		{
-			var sut = new AddAnimeRequestParameters(CredentialContextFixture.CredentialContext, ID, _data);
+			var sut = new AddAnimeRequestParameters(CredentialContextFixture.CredentialContext, AnimeValuesFixture.AnimeId, AnimeValuesFixture.Data);
 
 			Assert.Equal("POST", sut.HttpMethod);
 		}
@@ -68,12 +40,12 @@ namespace Project.MyAnimeList.Test.Tests
 		[Fact]
 		public void AddAnimeRequestParametersBaseUriIsBuiltBasedOnIdPassed()
 		{
-			var requestParameters = new AddAnimeRequestParameters(CredentialContextFixture.CredentialContext, ID, _data);
+			var requestParameters = new AddAnimeRequestParameters(CredentialContextFixture.CredentialContext, AnimeValuesFixture.AnimeId, AnimeValuesFixture.Data);
 			var sut = new RequestUriBuilder(requestParameters);
 
 			var actualUri = sut.GetRequestUri();
 
-			Assert.Equal($"http://myanimelist.net/api/animelist/add/{ID}.xml", actualUri);
+			Assert.Equal($"http://myanimelist.net/api/animelist/add/{AnimeValuesFixture.AnimeId}.xml", actualUri);
 		}
 
 		[Fact]
@@ -82,7 +54,7 @@ namespace Project.MyAnimeList.Test.Tests
 			var sut = _animeListMethodsFixture.AnimeListMethods;
 
 			const string expectedSubstring = "Created";
-			var actualString = sut.AddAnime(ID, _data);
+			var actualString = sut.AddAnime(AnimeValuesFixture.AnimeId, AnimeValuesFixture.Data);
 
 			Assert.Contains(expectedSubstring, actualString);
 		}
@@ -93,7 +65,7 @@ namespace Project.MyAnimeList.Test.Tests
 			var sut = _animeListMethodsFixture.AnimeListMethods;
 
 			const string expectedSubstring = "Created";
-			var actualString = sut.AddAnime(ID, _animeValuesFixture.Values);
+			var actualString = sut.AddAnime(AnimeValuesFixture.AnimeId, _animeValuesFixture.Values);
 
 			Assert.Contains(expectedSubstring, actualString);
 		}
@@ -103,7 +75,7 @@ namespace Project.MyAnimeList.Test.Tests
 		{
 			var sut = _animeListMethodsFixture.AnimeListMethods;
 
-			var actualResponseString = sut.UpdateAnime(ID, _data);
+			var actualResponseString = sut.UpdateAnime(AnimeValuesFixture.AnimeId, AnimeValuesFixture.Data);
 			_output.WriteLine("Actual: {0}", actualResponseString);
 
 			const string expected = "Updated";
@@ -115,7 +87,7 @@ namespace Project.MyAnimeList.Test.Tests
 		{
 			var sut = _animeListMethodsFixture.AnimeListMethods;
 
-			var actualResponseString = sut.UpdateAnime(ID, _animeValuesFixture.Values);
+			var actualResponseString = sut.UpdateAnime(AnimeValuesFixture.AnimeId, _animeValuesFixture.Values);
 			_output.WriteLine("Actual: {0}", actualResponseString);
 
 			const string expected = "Updated";
@@ -127,7 +99,7 @@ namespace Project.MyAnimeList.Test.Tests
 		{
 			var sut = _animeListMethodsFixture.AnimeListMethods;
 
-			var actualResponseString = sut.DeleteAnime(ID);
+			var actualResponseString = sut.DeleteAnime(AnimeValuesFixture.AnimeId);
 			_output.WriteLine("Actual: {0}", actualResponseString);
 
 			Assert.Equal("Deleted", actualResponseString);
