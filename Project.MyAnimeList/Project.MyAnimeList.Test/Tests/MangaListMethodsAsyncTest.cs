438236520ced6cdb2249f2a4f7f975861e37c190
dance2die <dance2die@hotmail.com>
2015-10-11 19:26:18 -0400

    created skeleton codes for "MangaListMethodsAsyncTest"

---
+++ Project.MyAnimeList/Project.MyAnimeList.Test/Tests/MangaListMethodsAsyncTest.cs
@@ -0,0 +1,54 @@
+﻿using MyAnimeListSharp.Auth;
+using MyAnimeListSharp.Core;
+using Project.MyAnimeList.Test.Fixture;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Project.MyAnimeList.Test.Tests
+{
+	public class MangaListMethodsAsyncTest :
+		CredentialCollectionTest,
+		IClassFixture<MangaValuesFixture>,
+		IClassFixture<MangaListMethodsAsyncFixture>
+	{
+		private ITestOutputHelper _output;
+		private MangaValuesFixture _mangaValuesFixture;
+		private MangaListMethodsAsyncFixture _mangaListMethodsAsyncFixture;
+
+		public MangaListMethodsAsyncTest(
+			CredentialContextFixture credentialContextFixture,
+			ITestOutputHelper output,
+			MangaValuesFixture mangaValuesFixture,
+			MangaListMethodsAsyncFixture mangaListMethodsAsyncFixture)
+				: base(credentialContextFixture)
+		{
+			_output = output;
+			_mangaValuesFixture = mangaValuesFixture;
+			_mangaListMethodsAsyncFixture = mangaListMethodsAsyncFixture;
+		}
+	}
+
+	public class MangaListMethodsAsyncFixture
+	{
+		public MangaListMethodsAsync MangaListMethods { get; set; }
+
+		public MangaListMethodsAsyncFixture()
+			: this(new CredentialContextFixture())
+		{
+		}
+
+		public MangaListMethodsAsyncFixture(CredentialContextFixture credentialContextFixture)
+		{
+			MangaListMethods = new MangaListMethodsAsync(credentialContextFixture.CredentialContext);
+		}
+
+	}
+
+	public class MangaListMethodsAsync : MyAnimeListMethodsAsync
+	{
+		public MangaListMethodsAsync(ICredentialContext credentialContext) 
+			: base(credentialContext)
+		{
+		}
+	}
+}
